f0c09d71dfca49fad5b238e8ea0d0492c931e684
0101chaitanya <0101chaitanya@gmail.com>
2021-09-15 15:09:32 +0530

    port change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 const connect = require("./config/db");
 const http = require("http");
 const socket = require("socket.io");
-const server = app.listen(3000, () => {
+const server = app.listen(3002, () => {
   console.log("listening on port 3000...");
 });
 
